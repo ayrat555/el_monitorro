35c2b515afe911a888af077c03d0fb9796fd6578
Ayrat Badykov <ayratin555@gmail.com>
2020-01-04 15:38:09 +0300

    add processing of russian texts

--- src/keyword_tagger.rs
+++ src/keyword_tagger.rs
@@ -1,12 +1,18 @@
 use rake::*;
 
+#[derive(Debug)]
 pub struct KeywordTagger {
-    pub text: String
+    pub text: String,
+    pub stop_words: Option<StopWords>
 }
 
 impl KeywordTagger {
     pub fn process(&self) -> Vec<KeywordScore> {
-        let stop_words = StopWords::from_file("./support/english_stopwords").unwrap();
+        let stop_words = match &self.stop_words {
+            None => StopWords::from_file("./support/english_stopwords").unwrap(),
+            Some(stop_words) => stop_words.clone()
+        };
+
         let rake = Rake::new(stop_words);
 
         rake.run(&self.text)
