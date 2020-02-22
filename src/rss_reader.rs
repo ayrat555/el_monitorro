494c26dca6e796c959857a16141a3144f07c5dc2
Ayrat Badykov <ayratin555@gmail.com>
2020-02-22 10:59:19 +0300

    make RssReader mockable

--- src/rss_reader.rs
+++ src/rss_reader.rs
@@ -1,11 +1,38 @@
+use mockall::*;
 use rss::Channel;
+use rss::Error;
 
 pub struct RssReader {
     pub url: String,
 }
 
-impl RssReader {
-    pub fn read(&self) -> Channel {
-        Channel::from_url(&self.url).unwrap()
+#[automock]
+pub trait ReadRSS {
+    fn read_rss(&self) -> Result<Channel, Error>;
+}
+
+impl ReadRSS for RssReader {
+    fn read_rss(&self) -> Result<Channel, Error> {
+        Channel::from_url(&self.url)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::MockReadRSS;
+    use super::ReadRSS;
+    use rss::Channel;
+    use std::fs;
+    use std::str::FromStr;
+
+    #[test]
+    fn it_mocks_read_rss_trait() {
+        let mut mock = MockReadRSS::new();
+        let xml_feed = fs::read_to_string("./tests/support/rss_feed_example.xml").unwrap();
+
+        mock.expect_read_rss()
+            .returning(move || Channel::from_str(&xml_feed));
+
+        assert!(mock.read_rss().is_ok());
     }
 }
