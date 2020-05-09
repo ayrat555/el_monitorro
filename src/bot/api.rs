39be9a3f8bc6dbf95ef77fcf80b171b5e8b83f61
Ayrat Badykov <ayratin555@gmail.com>
2020-05-09 13:12:16 +0300

    add /subscribe command

--- src/bot/api.rs
+++ src/bot/api.rs
@@ -1,133 +1,77 @@
-use std::env;
-use std::time::Duration;
-
+use crate::bot::logic;
+use crate::bot::logic::SubscriptionError;
+use crate::db;
+use crate::db::telegram::NewTelegramChat;
 use futures::StreamExt;
+use std::env;
 use telegram_bot::prelude::*;
-use telegram_bot::{Api, Error, Message, MessageKind, ParseMode, UpdateKind};
-use tokio::time::delay_for;
+use telegram_bot::{Api, Error, Message, MessageChat, MessageKind, UpdateKind};
 
-async fn test_message(api: Api, message: Message) -> Result<(), Error> {
-    api.send(message.text_reply("Simple message")).await?;
+static SUBSCRIBE: &str = "/subscribe";
 
-    let mut reply = message.text_reply("`Markdown message`");
-    api.send(reply.parse_mode(ParseMode::Markdown)).await?;
-
-    let mut reply = message.text_reply("<b>Bold HTML message</b>");
-
-    api.send(reply.parse_mode(ParseMode::Html)).await?;
-    Ok(())
-}
-
-async fn test_preview(api: Api, message: Message) -> Result<(), Error> {
-    api.send(message.text_reply("Message with preview https://telegram.org"))
-        .await?;
-
-    let mut reply = message.text_reply("Message without preview https://telegram.org");
-
-    api.send(reply.disable_preview()).await?;
-    Ok(())
-}
-
-async fn test_reply(api: Api, message: Message) -> Result<(), Error> {
-    api.send(message.text_reply("Reply to message")).await?;
-    api.send(message.chat.text("Text to message chat")).await?;
-
-    api.send(message.from.text("Private text")).await?;
-    Ok(())
-}
-
-async fn test_forward(api: Api, message: Message) -> Result<(), Error> {
-    api.send(message.forward(&message.chat)).await?;
-
-    api.send(message.forward(&message.from)).await?;
-    Ok(())
-}
-
-async fn test_edit_message(api: Api, message: Message) -> Result<(), Error> {
-    let message1 = api.send(message.text_reply("Round 1")).await?;
-
-    delay_for(Duration::from_secs(2)).await;
-
-    let message2 = api.send(message1.edit_text("Round 2")).await?;
-
-    delay_for(Duration::from_secs(4)).await;
-
-    api.send(message2.edit_text("Round 3")).await?;
-    Ok(())
-}
-
-async fn test_get_chat(api: Api, message: Message) -> Result<(), Error> {
-    let chat = api.send(message.chat.get_chat()).await?;
-    println!("{:?}", chat);
-    println!("{:?}", message);
-    api.send(chat.text(format!("Chat id {}", chat.id())))
-        .await?;
-    Ok(())
-}
-
-async fn test_get_chat_administrators(api: Api, message: Message) -> Result<(), Error> {
-    let administrators = api.send(message.chat.get_administrators()).await?;
-    let mut response = Vec::new();
-    for member in administrators {
-        response.push(member.user.first_name.clone())
+impl From<MessageChat> for NewTelegramChat {
+    fn from(message_chat: MessageChat) -> Self {
+        if let MessageChat::Private(chat) = message_chat {
+            NewTelegramChat {
+                id: chat.id.into(),
+                kind: "private".to_string(),
+                username: chat.username,
+                first_name: Some(chat.first_name),
+                last_name: chat.last_name,
+            }
+        } else {
+            unimplemented!()
+        }
     }
-    api.send(message.text_reply(format!("Administrators: {}", response.join(", "))))
-        .await?;
-    Ok(())
 }
 
-async fn test_get_chat_members_count(api: Api, message: Message) -> Result<(), Error> {
-    let count = api.send(message.chat.get_members_count()).await?;
-    api.send(message.text_reply(format!("Members count: {}", count)))
-        .await?;
-    Ok(())
-}
+async fn subscribe(api: Api, message: Message, data: String) -> Result<(), Error> {
+    let chat = api.send(message.chat.get_chat()).await?;
+    let response = match logic::create_subscription(
+        &db::establish_connection(),
+        message.chat.into(),
+        Some(data.clone()),
+    ) {
+        Ok(_subscription) => format!("Successfully subscribed to {}", data),
+        Err(SubscriptionError::DbError(_)) => {
+            "Something went wrong with the bot's storage".to_string()
+        }
+        Err(SubscriptionError::InvalidRssUrl) => "Invalid url".to_string(),
+        Err(SubscriptionError::RssUrlNotProvided) => "Url is not provided".to_string(),
+        Err(SubscriptionError::SubscriptionAlreadyExists) => {
+            "Susbscription already exists".to_string()
+        }
+        Err(SubscriptionError::SubscriptionCountLimit) => {
+            "You exceeded the number of subscriptins".to_string()
+        }
+        Err(SubscriptionError::TelegramError) => "Something went wrong with Telegram".to_string(),
+    };
 
-async fn test_get_chat_member(api: Api, message: Message) -> Result<(), Error> {
-    let member = api.send(message.chat.get_member(&message.from)).await?;
-    let first_name = member.user.first_name.clone();
-    let status = member.status;
-    api.send(message.text_reply(format!("Member {}, status {:?}", first_name, status)))
-        .await?;
-    Ok(())
-}
-
-async fn test_get_user_profile_photos(api: Api, message: Message) -> Result<(), Error> {
-    let photos = api.send(message.from.get_user_profile_photos()).await?;
-
-    api.send(message.text_reply(format!("Found photos: {}", photos.total_count)))
-        .await?;
-    Ok(())
-}
-
-async fn test_leave(api: Api, message: Message) -> Result<(), Error> {
-    api.send(message.chat.leave()).await?;
+    api.send(chat.text(response)).await?;
     Ok(())
 }
 
 async fn test(api: Api, message: Message) -> Result<(), Error> {
     match message.kind {
-        MessageKind::Text { ref data, .. } => match data.as_str() {
-            "/subscribe" => test_get_chat(api, message).await?,
-            // "/preview" => test_preview(api, message).await?,
-            // "/reply" => test_reply(api, message).await?,
-            // "/forward" => test_forward(api, message).await?,
-            // "/edit-message" => test_edit_message(api, message).await?,
-            // "/get_chat" => test_get_chat(api, message).await?,
-            // "/get_chat_administrators" => test_get_chat_administrators(api, message).await?,
-            // "/get_chat_members_count" => test_get_chat_members_count(api, message).await?,
-            // "/get_chat_member" => test_get_chat_member(api, message).await?,
-            // "/get_user_profile_photos" => test_get_user_profile_photos(api, message).await?,
-            // "/leave" => test_leave(api, message).await?,
-            _ => (),
-        },
+        MessageKind::Text { ref data, .. } => {
+            let command = data.as_str();
+
+            if command.contains(SUBSCRIBE) {
+                let argument = parse_argument(command, SUBSCRIBE);
+                subscribe(api, message, argument).await?;
+            }
+        }
         _ => (),
     };
 
     Ok(())
 }
 
-#[tokio::main]
+fn parse_argument(full_command: &str, command: &str) -> String {
+    full_command.replace(command, "").trim().to_string()
+}
+
+#[tokio::main(max_threads = 10)]
 pub async fn start_bot() -> Result<(), Error> {
     let token = env::var("TELEGRAM_BOT_TOKEN").expect("TELEGRAM_BOT_TOKEN not set");
 
