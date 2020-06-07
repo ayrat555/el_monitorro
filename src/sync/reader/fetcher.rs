d46c3c63c8a9d518fbd35e1a227c534a0c871bfb
Ayrat Badykov <ayratin555@gmail.com>
2020-06-07 18:35:24 +0300

    use updated field is published field is empty

--- src/sync/reader/fetcher.rs
+++ src/sync/reader/fetcher.rs
@@ -35,7 +35,8 @@ impl From<Feed> for FetchedFeed {
             .into_iter()
             .filter(|item| !item.links.is_empty())
             .map(|item| {
-                let pub_date: DateTime<Utc> = parse_time(item.published);
+                let pub_date: DateTime<Utc> = parse_time(item.published, item.updated);
+
                 FetchedFeedItem {
                     title: item.title.map_or_else(|| "".to_string(), |s| s.content),
                     description: item.summary.map(|s| s.content),
@@ -75,9 +76,12 @@ impl From<Feed> for FetchedFeed {
     }
 }
 
-fn parse_time(pub_date: Option<DateTime<Utc>>) -> DateTime<Utc> {
+fn parse_time(pub_date: Option<DateTime<Utc>>, updated: Option<DateTime<Utc>>) -> DateTime<Utc> {
     match pub_date {
-        None => db::current_time(),
+        None => match updated {
+            Some(value) => value,
+            None => db::current_time(),
+        },
         Some(value) => value,
     }
 }
@@ -96,7 +100,102 @@ mod tests {
 
         let fetched_feed: FetchedFeed = feed.into();
 
-        let expected_result = FetchedFeed { title: "World".to_string(), link: "".to_string(), description: "NPR world news, international art and culture, world business and financial markets, world economy, and global trends in health, science and technology. Subscribe to the World Story of the Day podcast and RSS feed.".to_string(), feed_type: "json".to_string(), items: vec![FetchedFeedItem { title: "Trump Says U.S. Will Withdraw From WHO. Does He Have The Authority To Do It?".to_string(), description: Some("<p>In a press conference on Friday, the president said he would immediately sever ties — and funding — to the World Health Organization because of its relationship with China.</p><img src=\'https://media.npr.org/include/images/tracking/npr-rss-pixel.png?story=865816855\' />".to_string()), link: "https://www.npr.org/sections/goatsandsoda/2020/05/29/865816855/trump-says-u-s-will-withdraw-from-who-does-he-have-the-authority-to-do-it?utm_medium=JSONFeed&utm_campaign=world".to_string(), author: Some("Pien Huang".to_string()), guid: Some("865816855".to_string()), publication_date: DateTime::parse_from_rfc3339("2020-05-29T23:30:03Z").unwrap().into() }, FetchedFeedItem { title: "France Eases Some Pandemic Restrictions And Will Reopen Restaurants, Bars And Parks".to_string(), description: Some("<p>\"It will be so nice to be able to go lie on the grass in a park and have a picnic or to sit at a sidewalk cafe again,\" says a Paris resident. Restaurants and bars will reopen with restrictions June 2.</p><img src=\'https://media.npr.org/include/images/tracking/npr-rss-pixel.png?story=864892887\' />".to_string()), link: "https://www.npr.org/sections/coronavirus-live-updates/2020/05/29/864892887/france-eases-some-pandemic-restrictions-and-will-reopen-restaurants-bars-and-par?utm_medium=JSONFeed&utm_campaign=world".to_string(), author: Some("Eleanor Beardsley".to_string()), guid: Some("864892887".to_string()), publication_date: DateTime::parse_from_rfc3339("2020-05-29T20:00:34Z").unwrap().into() }, FetchedFeedItem { title: "Moscow Doubles Last Month\'s Coronavirus Death Toll Amid Suspicions Of Undercounting".to_string(), description: Some("<p>Media reports and analysts have questioned the accuracy of Russia\'s mortality figures for the virus. Moscow\'s Health Department now says 1,561 people died in April due to the coronavirus.</p><img src=\'https://media.npr.org/include/images/tracking/npr-rss-pixel.png?story=865044503\' />".to_string()), link: "https://www.npr.org/sections/coronavirus-live-updates/2020/05/29/865044503/moscow-doubles-last-months-coronavirus-death-toll-amid-suspicions-of-undercounti?utm_medium=JSONFeed&utm_campaign=world".to_string(), author: Some("Jason Slotkin".to_string()), guid: Some("865044503".to_string()), publication_date: DateTime::parse_from_rfc3339("2020-05-29T19:35:00Z").unwrap().into() }] };
+        let expected_result = FetchedFeed {
+            title: "World".to_string(),
+            link: "".to_string(),
+            description: "NPR world news, international art and culture, world business and financial markets, world economy, and global trends in health, science and technology. Subscribe to the World Story of the Day podcast and RSS feed.".into(),
+            feed_type: "json".to_string(),
+            items: vec![
+                FetchedFeedItem {
+                    title: "Trump Says U.S. Will Withdraw From WHO. Does He Have The Authority To Do It?".to_string(),
+                    description: Some("In a press conference on Friday, the president said he would immediately sever ties — and funding — to the World Health Organization because of its relationship with China.".to_string()),
+                    link: "https://www.npr.org/sections/goatsandsoda/2020/05/29/865816855/trump-says-u-s-will-withdraw-from-who-does-he-have-the-authority-to-do-it?utm_medium=JSONFeed&utm_campaign=world".to_string(),
+                    author: Some("Pien Huang".to_string()),
+                    guid: Some("865816855".to_string()),
+                    publication_date: DateTime::parse_from_rfc3339("2020-05-29T23:30:03Z").unwrap().into() },
+                FetchedFeedItem {
+                    title: "France Eases Some Pandemic Restrictions And Will Reopen Restaurants, Bars And Parks".to_string(),
+                    description: Some("\"It will be so nice to be able to go lie on the grass in a park and have a picnic or to sit at a sidewalk cafe again,\" says a Paris resident. Restaurants and bars will reopen with restrictions June 2.".to_string()),
+                    link: "https://www.npr.org/sections/coronavirus-live-updates/2020/05/29/864892887/france-eases-some-pandemic-restrictions-and-will-reopen-restaurants-bars-and-par?utm_medium=JSONFeed&utm_campaign=world".to_string(),
+                    author: Some("Eleanor Beardsley".to_string()),
+                    guid: Some("864892887".to_string()),
+                    publication_date: DateTime::parse_from_rfc3339("2020-05-29T20:00:34Z").unwrap().into() },
+                FetchedFeedItem {
+                    title: "Moscow Doubles Last Month\'s Coronavirus Death Toll Amid Suspicions Of Undercounting".to_string(),
+                    description: Some("Media reports and analysts have questioned the accuracy of Russia\'s mortality figures for the virus. Moscow\'s Health Department now says 1,561 people died in April due to the coronavirus.".to_string()),
+                    link: "https://www.npr.org/sections/coronavirus-live-updates/2020/05/29/865044503/moscow-doubles-last-months-coronavirus-death-toll-amid-suspicions-of-undercounti?utm_medium=JSONFeed&utm_campaign=world".to_string(),
+                    author: Some("Jason Slotkin".to_string()),
+                    guid: Some("865044503".to_string()),
+                    publication_date: DateTime::parse_from_rfc3339("2020-05-29T19:35:00Z").unwrap().into() }
+            ]
+        };
+
+        assert_eq!(expected_result, fetched_feed);
+    }
+
+    #[test]
+    fn it_converts_rss_feed_to_fetched_feed() {
+        let atom_feed = fs::read_to_string("./tests/support/rss_feed_example.xml").unwrap();
+        let feed = parser::parse(atom_feed.as_bytes()).unwrap();
+
+        let fetched_feed: FetchedFeed = feed.into();
+
+        let expected_result = FetchedFeed {
+            title: "FeedForAll Sample Feed".into(),
+            link: "".into(),
+            description: "RSS is a fascinating technology. The uses for RSS are expanding daily. Take a closer look at how various industries are using the benefits of RSS in their businesses.".into(),
+            feed_type: "rss".into(),
+            items: vec![
+                FetchedFeedItem {
+                    title: "RSS Solutions for Restaurants".into(),
+                    description: Some("<b>FeedForAll </b>helps Restaurant\'s communicate with customers. Let your customers know the latest specials or events.<br>\n<br>\nRSS feed uses include:<br>\n<i><font color=\"#FF0000\">Daily Specials <br>\nEntertainment <br>\nCalendar of Events </i></font>".into()),
+                    link: "http://www.feedforall.com/restaurant.htm".into(),
+                    author: Some("".into()),
+                    guid: Some("6d47a5c95ff5dfd84f2adb6d0d7adf18".into()),
+                    publication_date: DateTime::parse_from_rfc3339("2004-10-19T15:09:11Z").unwrap().into() },
+                FetchedFeedItem {
+                    title: "RSS Solutions for Schools and Colleges".into(),
+                    description: Some("FeedForAll helps Educational Institutions communicate with students about school wide activities, events, and schedules.<br>\n<br>\nRSS feed uses include:<br>\n<i><font color=\"#0000FF\">Homework Assignments <br>\nSchool Cancellations <br>\nCalendar of Events <br>\nSports Scores <br>\nClubs/Organization Meetings <br>\nLunches Menus </i></font>".into()),
+                    link: "http://www.feedforall.com/schools.htm".into(),
+                    author: Some("".into()),
+                    guid: Some("d9c417b0f272d224ffe4e9c6d8e52a86".into()),
+                    publication_date: DateTime::parse_from_rfc3339("2004-10-19T15:09:09Z").unwrap().into() },
+                FetchedFeedItem {
+                    title: "RSS Solutions for Computer Service Companies".into(),
+                    description: Some("FeedForAll helps Computer Service Companies communicate with clients about cyber security and related issues. <br>\n<br>\nUses include:<br>\n<i><font color=\"#0000FF\">Cyber Security Alerts <br>\nSpecials<br>\nJob Postings </i></font>".into()),
+                    link: "http://www.feedforall.com/computer-service.htm".into(),
+                    author: Some("".into()),
+                    guid: Some("f40702952140e6a4322ffdb1b5304fe".into()),
+                    publication_date: DateTime::parse_from_rfc3339("2004-10-19T15:09:07Z").unwrap().into() }
+            ]
+        };
+
+        assert_eq!(expected_result, fetched_feed);
+    }
+
+    #[test]
+    fn it_converts_atom_feed_to_fetched_feed() {
+        let atom_feed = fs::read_to_string("./tests/support/atom_feed_example.xml").unwrap();
+        let feed = parser::parse(atom_feed.as_bytes()).unwrap();
+
+        let fetched_feed: FetchedFeed = feed.into();
+
+        let expected_result = FetchedFeed {
+            title: "Example Feed".into(),
+            link: "".into(),
+            description: "".to_string(),
+            feed_type: "atom".to_string(),
+            items: vec![FetchedFeedItem {
+                title: "Atom-Powered Robots Run Amok".to_string(),
+                description: Some("Some text.".into()),
+                link: "http://example.org/2003/12/13/atom03".into(),
+                author: Some("".into()),
+                guid: Some("urn:uuid:1225c695-cfb8-4ebb-aaaa-80da344efa6a".into()),
+                publication_date: DateTime::parse_from_rfc3339("2003-12-13T18:30:02Z")
+                    .unwrap()
+                    .into(),
+            }],
+        };
 
         assert_eq!(expected_result, fetched_feed);
     }
