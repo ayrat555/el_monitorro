de1df73b0882a5a4b0709c35b7173a9dd6e05308
Ayrat Badykov <ayratin555@gmail.com>
2020-08-08 19:23:15 +0300

    changes related to rust 2018

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,8 @@
-#![feature(proc_macro_hygiene, decl_macro)]
-
 #[macro_use]
 extern crate diesel;
-extern crate log;
+
 #[macro_use]
 extern crate failure;
-extern crate atom_syndication;
-extern crate dotenv;
-extern crate feed_rs;
-extern crate futures;
-extern crate html2text;
-extern crate isahc;
-extern crate rss;
-extern crate serde_json;
-extern crate telegram_bot;
-extern crate tokio;
-extern crate url;
 
 pub mod bot;
 pub mod cleaner;
