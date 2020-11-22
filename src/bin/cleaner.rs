1e3229eff5c3f6d9972e96f07d5e10d1a32efa88
Ayrat Badykov <ayratin555@gmail.com>
2020-11-22 17:26:17 +0300

    use semaphored connection in cleaner

--- src/bin/cleaner.rs
+++ src/bin/cleaner.rs
@@ -1,11 +1,35 @@
 use dotenv::dotenv;
 use el_monitorro;
-use el_monitorro::cleaner::clean_job;
+use el_monitorro::cleaner::clean_job::CleanJob;
+use el_monitorro::db;
+use tokio::runtime;
+use tokio::time;
 
-#[tokio::main]
-async fn main() {
+fn main() {
     dotenv().ok();
     env_logger::init();
 
-    clean_job::clean().await;
+    let mut tokio_runtime = runtime::Builder::new()
+        .thread_name("-pool")
+        .threaded_scheduler()
+        .enable_all()
+        .build()
+        .unwrap();
+
+    tokio_runtime.block_on(async {
+        let mut interval = time::interval(std::time::Duration::from_secs(60 * 60 * 12));
+
+        loop {
+            interval.tick().await;
+
+            if db::semaphore().available_permits() == *db::pool_connection_number() {
+                match CleanJob::new().execute().await {
+                    Err(error) => log::error!("Failed to remove old feed items: {}", error.msg),
+                    Ok(_) => (),
+                }
+            } else {
+                log::error!("The previous clean job did not finish");
+            }
+        }
+    })
 }
