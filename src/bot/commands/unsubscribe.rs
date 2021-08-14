2a1161a21b714218ba0920eece0b050e25f89c1b
Ayrat Badykov <ayratin555@gmail.com>
2021-08-14 16:52:20 +0300

    add unsubscribe

--- src/bot/commands/unsubscribe.rs
+++ src/bot/commands/unsubscribe.rs
@@ -1,29 +1,170 @@
-// use super::Command;
-// use super::Message;
-// use diesel::r2d2::ConnectionManager;
-// use diesel::r2d2::Pool;
-// use diesel::Connection;
-// use diesel::PgConnection;
+use super::Command;
+use super::Message;
+use crate::bot::telegram_client::Api;
+use crate::db::feeds;
+use crate::db::telegram;
+use crate::db::telegram::NewTelegramSubscription;
+use diesel::r2d2::ConnectionManager;
+use diesel::r2d2::Pool;
+use diesel::PgConnection;
 
-// static COMMAND: &str = "/unsubscribe";
+static COMMAND: &str = "/unsubscribe";
 
-// impl Command for Unsubscribe {
-//     fn response(
-//         &self,
-//         db_pool: Pool<ConnectionManager<PgConnection>>,
-//         message: &Message,
-//     ) -> String {
-//         match self.fetch_db_connection(db_pool) {
-//             Ok(connection) => {
-//                 let text = message.text().unwrap();
-//                 let argument = self.parse_argument(&text);
-//                 self.subscribe(&connection, message, argument)
-//             }
-//             Err(error_message) => error_message,
-//         }
-//     }
+pub struct Unsubscribe {}
 
-//     fn command(&self) -> &str {
-//         COMMAND
-//     }
-// }
+enum DeleteSubscriptionError {
+    FeedNotFound,
+    ChatNotFound,
+    SubscriptionNotFound,
+    DbError,
+}
+
+impl Unsubscribe {
+    pub fn execute(db_pool: Pool<ConnectionManager<PgConnection>>, api: Api, message: Message) {
+        Self {}.execute(db_pool, api, message);
+    }
+
+    fn unsubscribe(&self, db_connection: &PgConnection, message: &Message, url: String) -> String {
+        match self.delete_subscription(db_connection, message, url.clone()) {
+            Ok(_) => format!("Successfully unsubscribed from {}", url),
+            Err(DeleteSubscriptionError::DbError) => format!("Failed to unsubscribe from {}", url),
+            _ => "The subscription does not exist".to_string(),
+        }
+    }
+
+    fn delete_subscription(
+        &self,
+        db_connection: &PgConnection,
+        message: &Message,
+        link: String,
+    ) -> Result<(), DeleteSubscriptionError> {
+        let feed = match feeds::find_by_link(db_connection, link) {
+            Some(feed) => feed,
+            None => return Err(DeleteSubscriptionError::FeedNotFound),
+        };
+
+        let chat = match telegram::find_chat(db_connection, message.chat().id()) {
+            Some(chat) => chat,
+            None => return Err(DeleteSubscriptionError::ChatNotFound),
+        };
+
+        let telegram_subscription = NewTelegramSubscription {
+            chat_id: chat.id,
+            feed_id: feed.id,
+        };
+
+        let _subscription = match telegram::find_subscription(db_connection, telegram_subscription)
+        {
+            Some(subscription) => subscription,
+            None => return Err(DeleteSubscriptionError::SubscriptionNotFound),
+        };
+
+        match telegram::remove_subscription(db_connection, telegram_subscription) {
+            Ok(_) => Ok(()),
+            _ => Err(DeleteSubscriptionError::DbError),
+        }
+    }
+
+    pub fn command() -> &'static str {
+        COMMAND
+    }
+}
+
+impl Command for Unsubscribe {
+    fn response(
+        &self,
+        db_pool: Pool<ConnectionManager<PgConnection>>,
+        message: &Message,
+    ) -> String {
+        match self.fetch_db_connection(db_pool) {
+            Ok(connection) => {
+                let text = message.text().unwrap();
+                let argument = self.parse_argument(&text);
+                self.unsubscribe(&connection, message, argument)
+            }
+            Err(error_message) => error_message,
+        }
+    }
+
+    fn command(&self) -> &str {
+        COMMAND
+    }
+}
+
+#[cfg(test)]
+mod unsubscribe_tests {
+    use super::Unsubscribe;
+    use crate::db;
+    use crate::db::feeds;
+    use crate::db::telegram;
+    use crate::db::telegram::NewTelegramChat;
+    use crate::db::telegram::NewTelegramSubscription;
+    use diesel::connection::Connection;
+    use frankenstein::Chat;
+    use frankenstein::Message;
+
+    #[test]
+    fn removes_subscription() {
+        let connection = db::establish_connection();
+        let link = "Link88".to_string();
+
+        connection.test_transaction::<(), (), _>(|| {
+            let new_chat = NewTelegramChat {
+                id: 42,
+                kind: "private".to_string(),
+                username: Some("Username".to_string()),
+                first_name: Some("First".to_string()),
+                last_name: Some("Last".to_string()),
+                title: None,
+            };
+            let chat = telegram::create_chat(&connection, new_chat).unwrap();
+            let feed = feeds::create(&connection, link.clone(), "rss".to_string()).unwrap();
+
+            let new_subscription = NewTelegramSubscription {
+                feed_id: feed.id,
+                chat_id: chat.id,
+            };
+
+            telegram::create_subscription(&connection, new_subscription).unwrap();
+
+            let result = telegram::fetch_chats_with_subscriptions(&connection, 1, 1).unwrap();
+
+            assert_eq!(result.len(), 1);
+            assert_eq!(result[0], chat.id);
+
+            let chat = Chat::new(42, "private".into());
+            let message = Message::new(1, 1, chat);
+
+            let result = Unsubscribe {}.unsubscribe(&connection, &message, link.clone());
+
+            assert_eq!(format!("Successfully unsubscribed from {}", link), result);
+
+            let result = telegram::fetch_chats_with_subscriptions(&connection, 1, 1).unwrap();
+
+            assert_eq!(result.len(), 0);
+
+            Ok(())
+        });
+    }
+
+    #[test]
+    fn returns_error_if_subscription_does_not_exist() {
+        let connection = db::establish_connection();
+        let link = "Link88".to_string();
+
+        connection.test_transaction::<(), (), _>(|| {
+            let chat = Chat::new(42, "private".into());
+            let message = Message::new(1, 1, chat);
+
+            let result = Unsubscribe {}.unsubscribe(&connection, &message, link.clone());
+
+            assert_eq!("The subscription does not exist", result);
+
+            let result = telegram::fetch_chats_with_subscriptions(&connection, 1, 1).unwrap();
+
+            assert_eq!(result.len(), 0);
+
+            Ok(())
+        });
+    }
+}
