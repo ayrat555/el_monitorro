4e83c124b41351929ce997191c97e0add88b5571
Ayrat Badykov <ayratin555@gmail.com>
2021-08-20 15:12:37 +0300

    Deliver subscriptions only with updates

--- src/sync/feed_sync_job.rs
+++ src/sync/feed_sync_job.rs
@@ -10,6 +10,7 @@ use crate::sync::reader::ReadFeed;
 use crate::sync::FetchedFeed;
 use chrono::Duration;
 use diesel::pg::PgConnection;
+use diesel::result::Error;
 use fang::typetag;
 use fang::Error as FangError;
 use fang::Runnable;
@@ -94,104 +95,124 @@ impl FeedSyncJob {
             Some(found_feed) => found_feed,
         };
 
-        match read_feed(&feed) {
+        match self.read_feed(&feed) {
             Ok(fetched_feed) => {
                 match feed_items::create(db_connection, feed.id, fetched_feed.items) {
-                    Err(err) => {
-                        error!(
-                            "Error: failed to create feed items for feed with id {}: {:?}",
-                            self.feed_id, err
-                        );
-
-                        let error = FeedSyncError::DbError {
-                            msg: format!("Error: failed to create feed items {:?}", err),
-                        };
-                        Err(error)
-                    }
-                    _ => match feeds::set_synced_at(
+                    Err(err) => self.format_sync_error(err),
+                    _ => self.set_synced_at(
                         db_connection,
-                        &feed,
-                        Some(fetched_feed.title),
-                        Some(fetched_feed.description),
-                    ) {
-                        Err(err) => {
-                            error!(
-                                "Error: failed to update synced_at for feed with id {}: {:?}",
-                                self.feed_id, err
-                            );
-
-                            let error = FeedSyncError::DbError {
-                                msg: format!("Error: failed to update synced_at {:?}", err),
-                            };
-
-                            Err(error)
-                        }
-                        _ => Ok(()),
-                    },
+                        feed,
+                        fetched_feed.title,
+                        fetched_feed.description,
+                    ),
                 }
             }
+            Err(err) => self.check_staleness(err, db_connection, feed),
+        }
+    }
+
+    fn format_sync_error(&self, err: Error) -> Result<(), FeedSyncError> {
+        error!(
+            "Error: failed to create feed items for feed with id {}: {:?}",
+            self.feed_id, err
+        );
+
+        let error = FeedSyncError::DbError {
+            msg: format!("Error: failed to create feed items {:?}", err),
+        };
+        Err(error)
+    }
+
+    fn set_synced_at(
+        &self,
+        db_connection: &PgConnection,
+        feed: Feed,
+        title: String,
+        description: String,
+    ) -> Result<(), FeedSyncError> {
+        match feeds::set_synced_at(db_connection, &feed, Some(title), Some(description)) {
             Err(err) => {
-                let created_at_or_last_synced_at = if feed.synced_at.is_some() {
-                    feed.synced_at.unwrap()
-                } else {
-                    feed.created_at
+                error!(
+                    "Error: failed to update synced_at for feed with id {}: {:?}",
+                    self.feed_id, err
+                );
+
+                let error = FeedSyncError::DbError {
+                    msg: format!("Error: failed to update synced_at {:?}", err),
                 };
 
-                if db::current_time() - Duration::hours(SYNC_FAILURE_LIMIT_IN_HOURS)
-                    < created_at_or_last_synced_at
-                {
-                    let error = set_error(db_connection, &feed, err);
+                Err(error)
+            }
+            _ => Ok(()),
+        }
+    }
 
-                    Err(error)
-                } else {
-                    Err(FeedSyncError::StaleError)
+    fn check_staleness(
+        &self,
+        err: FeedReaderError,
+        db_connection: &PgConnection,
+        feed: Feed,
+    ) -> Result<(), FeedSyncError> {
+        let created_at_or_last_synced_at = if feed.synced_at.is_some() {
+            feed.synced_at.unwrap()
+        } else {
+            feed.created_at
+        };
+
+        if db::current_time() - Duration::hours(SYNC_FAILURE_LIMIT_IN_HOURS)
+            < created_at_or_last_synced_at
+        {
+            let error = self.set_error(db_connection, &feed, err);
+
+            Err(error)
+        } else {
+            Err(FeedSyncError::StaleError)
+        }
+    }
+
+    fn set_error(
+        &self,
+        db_connection: &PgConnection,
+        feed: &Feed,
+        sync_error: FeedReaderError,
+    ) -> FeedSyncError {
+        match feeds::set_error(db_connection, feed, &format!("{:?}", sync_error)) {
+            Err(err) => {
+                error!(
+                    "Error: failed to set a sync error to feed with id {} {:?}",
+                    feed.id, err
+                );
+
+                FeedSyncError::DbError {
+                    msg: format!("Error: failed to set a sync error to feed {:?}", err),
                 }
             }
+            _ => FeedSyncError::FeedError {
+                msg: format!("Error: failed to fetch feed items {:?}", sync_error),
+            },
         }
     }
-}
 
-fn set_error(
-    db_connection: &PgConnection,
-    feed: &Feed,
-    sync_error: FeedReaderError,
-) -> FeedSyncError {
-    match feeds::set_error(db_connection, feed, &format!("{:?}", sync_error)) {
-        Err(err) => {
-            error!(
-                "Error: failed to set a sync error to feed with id {} {:?}",
-                feed.id, err
-            );
-
-            FeedSyncError::DbError {
-                msg: format!("Error: failed to set a sync error to feed {:?}", err),
+    fn read_feed(&self, feed: &Feed) -> Result<FetchedFeed, FeedReaderError> {
+        match feed.feed_type.as_str() {
+            "rss" => RssReader {
+                url: feed.link.clone(),
             }
-        }
-        _ => FeedSyncError::FeedError {
-            msg: format!("Error: failed to fetch feed items {:?}", sync_error),
-        },
-    }
-}
+            .read(),
 
-fn read_feed(feed: &Feed) -> Result<FetchedFeed, FeedReaderError> {
-    match feed.feed_type.as_str() {
-        "rss" => RssReader {
-            url: feed.link.clone(),
-        }
-        .read(),
+            "atom" => AtomReader {
+                url: feed.link.clone(),
+            }
+            .read(),
 
-        "atom" => AtomReader {
-            url: feed.link.clone(),
+            "json" => JsonReader {
+                url: feed.link.clone(),
+            }
+            .read(),
+            &_ => Err(FeedReaderError {
+                msg: "Unknown feed type".to_string(),
+            }),
         }
-        .read(),
-
-        "json" => JsonReader {
-            url: feed.link.clone(),
-        }
-        .read(),
-        &_ => Err(FeedReaderError {
-            msg: "Unknown feed type".to_string(),
-        }),
     }
 }
 
@@ -201,26 +222,37 @@ mod tests {
     use super::FeedSyncJob;
     use crate::db;
     use crate::db::{feed_items, feeds};
+    use diesel::Connection;
+    use mockito::mock;
 
     #[test]
-    #[ignore]
     fn it_saves_rss_items() {
+        let response = std::fs::read_to_string("./tests/support/rss_feed_example.xml").unwrap();
+        let path = "/feed";
+        let _m = mock("GET", path)
+            .with_status(200)
+            .with_body(response)
+            .create();
+        let link = format!("{}{}", mockito::server_url(), path);
         let connection = db::establish_connection();
-        let link = "https://www.feedforall.com/sample-feed.xml".to_string();
 
-        let feed = feeds::create(&connection, link, "rss".to_string()).unwrap();
-        let sync_job = FeedSyncJob { feed_id: feed.id };
+        connection.test_transaction::<(), (), _>(|| {
+            let feed = feeds::create(&connection, link, "rss".to_string()).unwrap();
+            let sync_job = FeedSyncJob { feed_id: feed.id };
 
-        sync_job.execute(&connection).unwrap();
+            sync_job.execute(&connection).unwrap();
 
-        let created_items = feed_items::find(&connection, feed.id).unwrap();
+            let created_items = feed_items::find(&connection, feed.id).unwrap();
 
-        assert_eq!(created_items.len(), 3);
+            assert_eq!(created_items.len(), 9);
 
-        let updated_feed = feeds::find(&connection, feed.id).unwrap();
-        assert!(updated_feed.synced_at.is_some());
-        assert!(updated_feed.title.is_some());
-        assert!(updated_feed.description.is_some());
+            let updated_feed = feeds::find(&connection, feed.id).unwrap();
+            assert!(updated_feed.synced_at.is_some());
+            assert!(updated_feed.title.is_some());
+            assert!(updated_feed.description.is_some());
+
+            Ok(())
+        })
     }
 
     #[test]
