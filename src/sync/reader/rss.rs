e1aa7b0b7a0d849d755773665de54a21618ccd0c
Ayrat Badykov <ayratin555@gmail.com>
2023-01-22 08:06:29 +0200

    support authors from dublincore (#295)
    * support authors from dublincore
    
    * fix clippy

--- src/sync/reader/rss.rs
+++ src/sync/reader/rss.rs
@@ -1,7 +1,12 @@
 use crate::db;
-use crate::sync::reader::{FeedReaderError, FetchedFeed, FetchedFeedItem, ReadFeed};
-use chrono::{DateTime, Utc};
+use crate::sync::reader::FeedReaderError;
+use crate::sync::reader::FetchedFeed;
+use crate::sync::reader::FetchedFeedItem;
+use crate::sync::reader::ReadFeed;
+use chrono::DateTime;
+use chrono::Utc;
 use rss::Channel;
+use rss::Item;
 
 pub struct RssReader {
     pub url: String,
@@ -37,7 +42,7 @@ impl From<Channel> for FetchedFeed {
                         .map_or_else(|| "".to_string(), |s| s.to_string()),
                     description: item.description().map(|s| s.to_string()),
                     link: item.link().unwrap().to_string(),
-                    author: item.author().map(|s| s.to_string()),
+                    author: author(item),
                     guid: item.guid().map(|s| s.value().to_string()),
                     publication_date: pub_date,
                 }
@@ -54,6 +59,24 @@ impl From<Channel> for FetchedFeed {
             items,
         }
     }
+}
+
+fn author(item: &Item) -> Option<String> {
+    let author = item.author().map(|s| s.to_string());
+
+    if author.is_some() {
+        return author;
+    }
+
+    if let Some(dublincore) = item.dublin_core_ext() {
+        let joined_creators = dublincore.creators().join(", ").trim().to_string();
+
+        if !joined_creators.is_empty() {
+            return Some(joined_creators);
+        }
+    }
+
+    None
 }
 
 fn parse_time(pub_date: Option<&str>) -> DateTime<Utc> {
@@ -81,5 +104,18 @@ mod tests {
         let fetched_feed: FetchedFeed = channel.into();
 
         assert_eq!(fetched_feed.title, "FeedForAll Sample Feed".to_string());
+    }
+
+    #[test]
+    fn it_fetched_author_from_dublincore() {
+        let xml_feed = fs::read_to_string("./tests/support/rss_dublincore_author.xml").unwrap();
+        let channel = Channel::from_str(&xml_feed).unwrap();
+
+        let fetched_feed: FetchedFeed = channel.into();
+
+        assert_eq!(
+            fetched_feed.items[0].author.as_ref().unwrap(),
+            "@FabrizioRomano"
+        );
     }
 }
